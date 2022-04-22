0b090e772be388e6a743e9c85cf3aa06ff66339b
sergeizhedulov <92210521+sergeizhedulov@users.noreply.github.com>
2022-04-22 23:20:09 +0300

    Add files via upload

--- migrations/2014_10_12_000000_create_users_table.php
+++ migrations/2014_10_12_000000_create_users_table.php
@@ -14,16 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id()->autoIncrement();
-            $table->string('name', 20)->nullable();;
-            $table->string('soname', 20)->nullable();;
+            $table->id();
+            $table->string('name', 20);
+            $table->string('soname', 20);
             $table->string('patronymic', 20);
-            $table->date('birth_day',10)->nullable();;
+            $table->date('birth_day',10);
             $table->string('city',20);
-            $table->string('phone_number',10)->unique();
+            $table->string('phone_number',10);
             $table->string('messengers');
-            $table->string('create_at')->nullable();;
+            $table->timestamps();
             $table->string('email',30)->unique();
+            $table->enum('user_status', ['Водитель', 'Пассажир'])->default('Пассажир');
         });
     }
 
